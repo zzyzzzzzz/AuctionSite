d76d6be22601a0bd44572c81060e230db3d593e0
Ziying <u7780416@anu.edu.au>
2024-10-05 20:42:08 +1000

    Adding filtering and ordering

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -1,33 +1,57 @@
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.RequestHelpers;
 
 namespace SearchService;
 
  
 [ApiController]
 [Route("api/search")]
-public class SearchService : ControllerBase
+public class SearchController : ControllerBase
 {
     [HttpGet]
     
-    public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm, 
-        int pageNumber = 1, int pageSize = 4)
+    public async Task<ActionResult<List<Item>>> SearchItems([FromQuery] SearchParams searchParams)
     {
-        var query = DB.PagedSearch<Item>(); //不用Find了 用PagedSearch
+        var query = DB.PagedSearch<Item, Item>(); //不用Find了 用PagedSearch
         
-        query.Sort(x => x.Ascending(a => a.Make));
+        //因为下面有新的sorting了所以不需要这个了
+        //query.Sort(x => x.Ascending(a => a.Make));
 
-        if(!string.IsNullOrEmpty(searchTerm))
-        {//if we do have the string
-        // 如果有搜索条件，打印出来
-            //Console.WriteLine($"Search term: {searchTerm}");
-
-            query.Match(Search.Full, searchTerm).SortByTextScore();
+        if(!string.IsNullOrEmpty(searchParams.SearchTerm))
+        {//if we do have the string  // 如果有搜索条件，打印出来//Console.WriteLine($"Search term: {searchTerm}");
+            query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
         }
 
-        query.PageNumber(pageNumber);
-        query.PageSize(pageSize);
+        //这里是filter 
+        query = searchParams.OrderBy switch
+        {
+            "make" => query.Sort(x => x.Ascending(a => a.Make)),
+            "new" => query.Sort(x => x.Descending(a => a.CreatedAt)),
+            _ => query.Sort(x => x.Ascending(a => a.AuctionEnd))
+        };
+
+        query = searchParams.FilterBy switch
+        {
+            "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
+            "endingSoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(6)
+            && x.AuctionEnd > DateTime.UtcNow),
+            _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow)
+        };
+
+        if(!string.IsNullOrEmpty(searchParams.Seller))
+        {
+            query.Match(x => x.Seller == searchParams.Seller);
+        }
+
+        if(!string.IsNullOrEmpty(searchParams.Winner))
+        {
+            query.Match(x => x.Winner == searchParams.Winner);
+        }
+
+        query.PageNumber(searchParams.PageNumber);
+        query.PageSize(searchParams.PageSize);
 
         var result = await query.ExecuteAsync();
 
@@ -35,7 +59,8 @@ public class SearchService : ControllerBase
         //Console.WriteLine($"Results Count: {result.Results.Count}, Total Count: {result.TotalCount}");
 
 
-        return Ok(new{
+        return Ok(new
+        {
             results = result.Results,
             pageCount = result.PageCount,
             totalCount = result.TotalCount
