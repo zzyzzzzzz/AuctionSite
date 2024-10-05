4d67fcae668e2a4750e90daffb0907c4cc597f63
Ziying <u7780416@anu.edu.au>
2024-10-05 18:56:16 +1000

    Adding pagenating

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -11,19 +11,35 @@ public class SearchService : ControllerBase
 {
     [HttpGet]
     
-    public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm)
+    public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm, 
+        int pageNumber = 1, int pageSize = 4)
     {
-        //然后去看mongodb的文档
-        var query = DB.Find<Item>();
+        var query = DB.PagedSearch<Item>(); //不用Find了 用PagedSearch
+        
         query.Sort(x => x.Ascending(a => a.Make));
 
         if(!string.IsNullOrEmpty(searchTerm))
         {//if we do have the string
+        // 如果有搜索条件，打印出来
+            //Console.WriteLine($"Search term: {searchTerm}");
+
             query.Match(Search.Full, searchTerm).SortByTextScore();
         }
 
+        query.PageNumber(pageNumber);
+        query.PageSize(pageSize);
+
         var result = await query.ExecuteAsync();
-        return result;
+
+        // 打印结果以帮助调试
+        //Console.WriteLine($"Results Count: {result.Results.Count}, Total Count: {result.TotalCount}");
+
+
+        return Ok(new{
+            results = result.Results,
+            pageCount = result.PageCount,
+            totalCount = result.TotalCount
+        });
     }
 
 }
