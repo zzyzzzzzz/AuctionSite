16726307b1c64564158cdea78095143c28f9a985
Ziying <u7780416@anu.edu.au>
2024-10-16 13:14:27 +1100

    Seeding data and adding register page

---
+++ src/IdentityService/Pages/Account/Register/Index.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityService.Pages.Register
+{
+    [SecurityHeaders]
+    [AllowAnonymous]
+    public class Index : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
