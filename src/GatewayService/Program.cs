aba984dbdfebce6742177a3fc7aa16d37ccf7312
Ziying <u7780416@anu.edu.au>
2024-10-22 15:36:02 +1100

    Add authentication and Fix errors

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -1,10 +1,26 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+//从auctionservice中拷贝过来的
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration["IdentityServiceUrl"];
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters.ValidateAudience = false;
+        options.TokenValidationParameters.NameClaimType = "username";
+    });
+
 var app = builder.Build();
 
 app.MapReverseProxy();
+
+//加上 authentication 和 authorization 因为我们的网关需要验证用户的token
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
